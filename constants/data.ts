ca18115bdc1b2944eb98d57e5bde8522d4b39bc8
sufiyan2401 <sufiyan240107@gmail.com>
2025-07-27 04:13:48 +0500

    Add NavigationItem interface and navigationItems constant for app navigation

---
+++ constants/data.ts
@@ -0,0 +1,9 @@
+import { NavigationItem } from "./types";
+
+export const navigationItems : NavigationItem[] = [
+  { id: 'apps', icon: '/images/icons/square.svg', label: 'Apps' },
+  { id: 'calendar', icon: '/images/icons/calendar.svg', label: 'Calendar' },
+  { id: 'video', icon: '/images/icons/video.svg', label: 'Video Call' },
+  { id: 'chat', icon: '/images/icons/chat.svg', label: 'Chat' },
+  { id: 'settings', icon: '/images/icons/settings.svg', label: 'Settings' },
+];
---
+++ constants/types.ts
@@ -0,0 +1,6 @@
+
+export interface NavigationItem {
+  id: string;
+  icon: string;
+  label: string;
+}
